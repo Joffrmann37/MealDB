35b8fc013af139184f944052ca57c5ad77010f61
Joffrey Mann <joffrey@jfammobility.com>
2024-04-21 16:12:25 -0400

    Initial Commit

---
+++ MealDB/MealDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDBApp.swift
+//  MealDB
+//
+//  Created by Joffrey Mann on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MealDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
